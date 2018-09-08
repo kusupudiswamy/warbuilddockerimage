d20fbd0e071115f0bb89c0e2a625622b2bcd2dd4
kusupudiswamy <39132487+kusupudiswamy@users.noreply.github.com>
2018-09-08 15:19:53 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ sudo cd /opt/swamy
 buildNumber=$1
 pwd
 sudo docker build -t learn:$buildNumber /opt/swamy
-sudo docker tag learn:buildNumber kusupudiswamy/learn:$buildNumber
+sudo docker tag learn:$buildNumber kusupudiswamy/learn:$buildNumber
 sudo docker push kusupudiswamy/learn:$buildNumber
