51eba648aa55eb863c9b88a6eecf991f9f6660c7
kusupudiswamy <39132487+kusupudiswamy@users.noreply.github.com>
2018-10-24 12:33:34 +0530

    update learn to learning

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ ls /opt/swamy
 sudo cd /opt/swamy
 buildNumber=$1
 pwd
-sudo docker build -t learn:$buildNumber /opt/swamy
-sudo docker tag learn:$buildNumber kusupudiswamy/learn:$buildNumber
-sudo docker push kusupudiswamy/learn:$buildNumber
+sudo docker build -t learning:$buildNumber /opt/swamy
+sudo docker tag learning:$buildNumber kusupudiswamy/learning:$buildNumber
+sudo docker push kusupudiswamy/learning:$buildNumber
