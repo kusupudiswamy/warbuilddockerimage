7730245539e8f0be3b3906a2414f402d35e1f689
kusupudiswamy <39132487+kusupudiswamy@users.noreply.github.com>
2018-10-24 12:46:27 +0530

    Update deploying.sh

--- deploying.sh
+++ deploying.sh
@@ -2,7 +2,7 @@
 buildNumber=$1
 exists=$(docker service ls|grep learn)
 if [ ! "$exists"]; then
- docker service create --name su -p 8888:8080 kusupudiswamy/learn:$buildNumber
+ docker service create --name su -p 8888:8080 kusupudiswamy/learning:$buildNumber
  else
- docker service update su --image kusupudiswamy/learn:$buildNumber
+ docker service update su --image kusupudiswamy/learning:$buildNumber
  fi
